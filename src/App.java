54dc09272e2f8c59f96fe9afe62b1e484d9a72b6
harits.nurrois@pawoon.com <harits.nurrois@it.pawoon.com>
2022-12-03 15:52:51 +0700

    updae

--- src/App.java
+++ src/App.java
@@ -1,6 +1,14 @@
+import java.time.*;
+
 public class App {
     public static void main(String[] args) throws Exception {
-        Dosen ds = new Dosen();
-        ds.masukKerja();
+        Pegawai p1 = new Pegawai("P01", "Rosihan Ari", 2, LocalDate.of(1979, 9, 1));
+        p1.printPegawai();
+        // contoh obyek data pegawai dosen
+        Dosen p2 = new Dosen("P02", "Dwi Amalia Fitriani", 1, LocalDate.of(1979, 9, 1),"1234");
+        p2.printPegawai();
+        // contoh obyek data pegawai nondosen
+        NonDosen p3 = new NonDosen("P03", "Faza Fauzan K", 1, LocalDate.of(2008, 1, 2));
+        p3.printPegawai();
     }
 }
